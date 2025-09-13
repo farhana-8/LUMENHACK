15b1ea888fb32f61f6d6fa2f9fad6dc545651c50
RUDDUM SURAIAH KHAISAR <150116171+suraiahkhaisar@users.noreply.github.com>
2025-09-13 12:23:16 +0530

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -2,10 +2,12 @@
 const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // optional link to a user
   name: { type: String, required: true },
-  email: { type: String, required: true },
-  message: { type: String, required: true },
+  emailId: { type: String, required: true },       // changed to emailId
+  companyName: { type: String },                   // optional company field
+  subject: { type: String, required: true },       // subject of the message
+  message: { type: String, required: true },       // detailed message
 }, { timestamps: true });
 
 module.exports = mongoose.model("Contact", contactSchema);
