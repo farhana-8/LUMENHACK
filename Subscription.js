057bfda6a5d2c66d6e01bfbfea9a89a0d1966744
RUDDUM SURAIAH KHAISAR <150116171+suraiahkhaisar@users.noreply.github.com>
2025-09-13 11:28:46 +0530

    Update Subscription.js

--- Subscription.js
+++ Subscription.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const subscriptionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  plan: { type: String, enum: ["free", "basic", "premium"], default: "free" },
+  plan: { type: String, enum: ["free", "basic", "premium", "student"], default: "free" },
   startDate: { type: Date, default: Date.now },
   endDate: { type: Date },
   isActive: { type: Boolean, default: true },
